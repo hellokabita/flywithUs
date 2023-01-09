3bdd8fa477e0aee56d688f3945e53ca496de4b86
hellokabita <kabitabk959@gmail.com>
2023-01-09 16:56:20 +0545

    creating navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -25,7 +25,7 @@ import 'bootstrap/dist/css/bootstrap.css';
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
+        <a class="nav-link" href="#">Login here</a>
       </li>
 
       <li class="nav-item">
