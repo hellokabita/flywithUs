7ae7dc26f5572ec4fcf046ddbb44ad8b4b449ce7
hellokabita <kabitabk959@gmail.com>
2023-01-18 09:58:36 +0545

    added homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from 'react-router-dom'
-import Navbar from './containers/navbar';
-import Home from './containers/home';
+import Navbar from './containers/navpage/navbar';
+import Home from './containers/homepage/home';
 import Contact from './containers/contact';
 import About from './containers/about';
 import Register from './containers/register';
-import Login from './containers/login';
+import Login from './containers/loginpage/login';
 
 
 const App=() => {
@@ -20,7 +20,7 @@ const App=() => {
       <Route exact path='/contact' element={<Contact/>}/>
       <Route exact path='/about' element={<About/>}/>
       <Route exact path='/register' element={<Register/>}/>
-      <Route exact path='/login' element={<Login/>}/>
+      <Route exact path='/' element={<Login/>}/>
      
     </Routes>
     </>
